ddec31aed4269772eaa5ae02ade9a71f0fdb11f4
OtecAbsolut <s@kyky.cloud>
2018-09-20 15:14:20 +0300

    Добавил условие на проверку папки и соответсвенно теперь вводит только папки..         if os.path.isdir(element):             print('{}. {}'.format(index, element))

--- Less5.py
+++ Less5.py
@@ -79,7 +79,8 @@ def list_dir ():
     print('****************************************')
     print('Список файлов:')
     for index, element in enumerate(buffer, start=1):
-        print('{}. {}'.format(index, element))
+        if os.path.isdir(element):
+            print('{}. {}'.format(index, element))
 
 if __name__ == '__main__':
     list_dir()
